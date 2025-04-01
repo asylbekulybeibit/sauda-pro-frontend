9731ac2168cdbbb5d84791ec0a63ae3f9d6dece9
asylbekulybeibit <asylbekulybeibit@gmail.com>
2025-04-01 15:34:31 +0500

    fgdfgf

--- src/components/cashier/ProductSearch.tsx
+++ src/components/cashier/ProductSearch.tsx
@@ -2,8 +2,6 @@ import React, { useState, useEffect, useRef } from 'react';
 import { cashierApi } from '../../services/cashierApi';
 import { Product } from '../../types/cashier';
 import styles from './ProductSearch.module.css';
-import Keyboard from 'react-simple-keyboard';
-import 'react-simple-keyboard/build/css/index.css';
 import { BsKeyboard } from 'react-icons/bs';
 
 interface ProductSearchProps {
@@ -20,9 +18,34 @@ const ProductSearch: React.FC<ProductSearchProps> = ({
   const [loading, setLoading] = useState(false);
   const [showResults, setShowResults] = useState(false);
   const [showKeyboard, setShowKeyboard] = useState(false);
+  const [isShift, setIsShift] = useState(false);
   const searchInputRef = useRef<HTMLInputElement>(null);
   const resultsRef = useRef<HTMLDivElement>(null);
-  const keyboardRef = useRef<any>(null);
+
+  const russianLayout = {
+    default: [
+      ['й', 'ц', 'у', 'к', 'е', 'н', 'г', 'ш', 'щ', 'з', 'х'],
+      ['ф', 'ы', 'в', 'а', 'п', 'р', 'о', 'л', 'д', 'ж', 'э'],
+      ['я', 'ч', 'с', 'м', 'и', 'т', 'ь', 'б', 'ю'],
+    ],
+    shift: [
+      ['Й', 'Ц', 'У', 'К', 'Е', 'Н', 'Г', 'Ш', 'Щ', 'З', 'Х'],
+      ['Ф', 'Ы', 'В', 'А', 'П', 'Р', 'О', 'Л', 'Д', 'Ж', 'Э'],
+      ['Я', 'Ч', 'С', 'М', 'И', 'Т', 'Ь', 'Б', 'Ю'],
+    ],
+  };
+
+  const handleKeyClick = (key: string) => {
+    if (key === 'backspace') {
+      setQuery((prev) => prev.slice(0, -1));
+    } else if (key === 'shift') {
+      setIsShift(!isShift);
+    } else if (key === 'space') {
+      setQuery((prev) => prev + ' ');
+    } else {
+      setQuery((prev) => prev + key);
+    }
+  };
 
   useEffect(() => {
     // Функция для обработки клика вне компонента
@@ -83,10 +106,6 @@ const ProductSearch: React.FC<ProductSearchProps> = ({
     }
   };
 
-  const handleKeyboardInput = (input: string) => {
-    setQuery(input);
-  };
-
   const toggleKeyboard = () => {
     setShowKeyboard(!showKeyboard);
   };
@@ -128,19 +147,113 @@ const ProductSearch: React.FC<ProductSearchProps> = ({
       )}
       {showKeyboard && (
         <div className={styles.virtualKeyboard}>
-          <Keyboard
-            keyboardRef={(r: any) => (keyboardRef.current = r)}
-            onChange={handleKeyboardInput}
-            onKeyPress={() => null}
-            layout={{
-              default: [
-                '1 2 3 4 5 6 7 8 9 0',
-                'й ц у к е н г ш щ з х ъ',
-                'ф ы в а п р о л д ж э',
-                'я ч с м и т ь б ю {bksp}',
-              ],
-            }}
-          />
+          <div className={styles.keyboardContainer}>
+            <div className={styles.leftSection}>
+              {(isShift ? russianLayout.shift : russianLayout.default).map(
+                (row, rowIndex) => (
+                  <div key={rowIndex} className={styles.keyboardRow}>
+                    {row.map((key) => (
+                      <button
+                        key={key}
+                        className={styles.key}
+                        onClick={() => handleKeyClick(key)}
+                      >
+                        {key}
+                      </button>
+                    ))}
+                  </div>
+                )
+              )}
+              <div className={styles.keyboardRow}>
+                <button
+                  className={`${styles.key} ${styles.funcKey}`}
+                  onClick={() => handleKeyClick('shift')}
+                >
+                  СИМ
+                </button>
+                <button className={`${styles.key} ${styles.funcKey}`}>
+                  ENG
+                </button>
+                <button
+                  className={`${styles.key} ${styles.spaceKey}`}
+                  onClick={() => handleKeyClick('space')}
+                ></button>
+                <button
+                  className={`${styles.key} ${styles.funcKey}`}
+                  onClick={() => handleKeyClick('shift')}
+                >
+                  ⇧
+                </button>
+              </div>
+            </div>
+            <button
+              className={styles.backspaceKey}
+              onClick={() => handleKeyClick('backspace')}
+            >
+              ←
+            </button>
+            <div className={styles.rightSection}>
+              <div className={styles.numericSection}>
+                <div className={styles.numericPad}>
+                  <div className={styles.keyboardRow}>
+                    {['1', '2', '3'].map((num) => (
+                      <button
+                        key={num}
+                        className={`${styles.key} ${styles.numKey}`}
+                        onClick={() => handleKeyClick(num)}
+                      >
+                        {num}
+                      </button>
+                    ))}
+                  </div>
+                  <div className={styles.keyboardRow}>
+                    {['4', '5', '6'].map((num) => (
+                      <button
+                        key={num}
+                        className={`${styles.key} ${styles.numKey}`}
+                        onClick={() => handleKeyClick(num)}
+                      >
+                        {num}
+                      </button>
+                    ))}
+                  </div>
+                  <div className={styles.keyboardRow}>
+                    {['7', '8', '9'].map((num) => (
+                      <button
+                        key={num}
+                        className={`${styles.key} ${styles.numKey}`}
+                        onClick={() => handleKeyClick(num)}
+                      >
+                        {num}
+                      </button>
+                    ))}
+                  </div>
+                  <div className={styles.keyboardRow}>
+                    <button
+                      className={`${styles.key} ${styles.numKey} ${styles.zeroKey}`}
+                      onClick={() => handleKeyClick('0')}
+                    >
+                      0
+                    </button>
+                    <button
+                      className={`${styles.key} ${styles.numKey}`}
+                      onClick={() => handleKeyClick('.')}
+                    >
+                      .
+                    </button>
+                  </div>
+                </div>
+                <div className={styles.sideButtons}>
+                  <button className={`${styles.key} ${styles.cancelKey}`}>
+                    ОТМЕНИТЬ
+                  </button>
+                  <button className={`${styles.key} ${styles.okKey}`}>
+                    OK
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       )}
     </div>
